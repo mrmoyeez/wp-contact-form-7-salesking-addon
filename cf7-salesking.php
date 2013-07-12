dd8f7d523df8db31646b5bbb95e4058f537c96a3
Georg Leciejewski <gl@salesking.eu>
2013-07-12 11:32:17 +0200

    change license to gpl

--- cf7-salesking.php
+++ cf7-salesking.php
@@ -6,7 +6,23 @@ Description: Add the power of SalesKing to Contact Form 7
 Author URI: http://www.salesking.eu
 Version: 1.0.0
 */
-// MIT License
+
+/*  Copyright 2013 SalesKing GmbH (email: support@salesking.eu)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
 
 define( 'WPCF7_SK_VERSION', '1.0.0' );
 
